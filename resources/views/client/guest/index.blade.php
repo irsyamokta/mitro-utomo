7c54a4ff0e9f9e92015fde033f4338c029c3620c
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-10-30 02:34:12 +0700

    Fix : UI Hero Add : Product section

--- resources/views/client/guest/index.blade.php
+++ resources/views/client/guest/index.blade.php
@@ -1,4 +1,6 @@
 @extends('index')
 @section('content')
     @include('client.guest.sections.hero')
+    @include('client.guest.sections.product-bio')
+    @include('client.guest.sections.product-degra')
 @endsection
---
+++ resources/views/client/guest/sections/product-degra.blade.php
@@ -0,0 +1,15 @@
+<section class="flex flex-col-reverse lg:flex-row-reverse justify-center items-center h-[100vh] overflow-hidden lg:px-12 relative">
+    <div class="absolute top-0">
+        <img src="{{ asset('assets/ornaments/ornaments-3.png') }}">
+    </div>
+    <div class="flex justify-center items-center">
+        <img src="{{ asset('assets/img/img-product-2.png') }}" class="w-56 md:w-[400px] lg:w-[800px]" alt="">
+    </div>
+    <div class="px-6 w-full max-w-screen-xl py-16 text-center lg:text-left">
+        <h1 class="text-2xl font-semibold tracking-wide text-accent md:text-5xl 2xl:text-6xl">ABG-DEGRA</h1>
+        <p class="lg:text-justify text-xs md:text-base font-regular mt-5 lg:mt-10 lg:pr-12 text-gray-700">ABG-Degra Adalah inokulan konsorsium mikroba pengurai (decomposer) unggul alami terutama sellulolitik. lignolitik dan ligno sellulolitik dengan kepadatan 109-10 cfu/gram inokulan (bakteri, aktinomycetes, ragi dan jamur). Efektif untuk menguraikan berbagai limbah organik, yaitu limbah perkotaan (sampah), rumah tangga, pertanian, peternakan, perikanan, dan limbah organik lainnya dari berbagai industri.</p>
+    </div>
+    <div class="absolute bottom-0">
+        <img src="{{ asset('assets/ornaments/ornaments-5.png') }}">
+    </div>
+</section>
