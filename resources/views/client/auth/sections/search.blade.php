6628242b4c70f94005ffd489415431c594e334ac
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-12-23 11:16:32 +0700

    Fix : Image path on page searching

--- resources/views/client/auth/sections/search.blade.php
+++ resources/views/client/auth/sections/search.blade.php
@@ -6,7 +6,8 @@
             <section id="product"
                 class="flex flex-col lg:flex-row-reverse justify-center items-center h-[100vh] overflow-hidden gap-12 lg:px-12 relative">
                 <div class="flex justify-center items-center">
-                    <img src="{{ asset('storage/' . $item->image) }}" class="w-56 md:w-[400px] lg:w-[800px]" alt="">
+                    <img src="{{ asset('mitroutomo/public/storage/' . $item->image) }}" class="w-56 md:w-[400px] lg:w-[800px]"
+                        alt="">
                 </div>
                 <div class="px-6 w-full max-w-screen-xl py-16 text-center lg:text-left">
                     <h1 class="text-2xl font-semibold tracking-wide text-accent md:text-5xl 2xl:text-6xl">
